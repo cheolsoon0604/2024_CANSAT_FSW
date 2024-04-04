564590082b773ff7488bec9a7c3d08ea34f59823
admin <cheolsoon0604@gmail.com>
2024-04-04 13:58:08 +0900

    IMU Update

--- cs_Common/cs_IMU.py
+++ cs_Common/cs_IMU.py
@@ -22,22 +22,67 @@ except:
 global IMU_serial
 global IMU_Buf
 
+# ----------------- Default Setting -----------------
 def IMU_Init() :
     # connect ebimu
     # IMU_serial = serial.Serial('/dev/ttyUSB0',115200, parity='N', timeout=0.001) #when connect to usb
     IMU_serial = serial.Serial('/dev/ttyAMA0', 115200, parity='N', timeout=0.001)  # when connect to GPIO pins (tx4,rx5)
+    IMU_reset()
+
+    IMU_Output_Rate_polling()
+    IMU_Output_Code_ASCII()
+    IMU_Output_Euler()
+
+    IMU_Set_Acc_ON()
+    IMU_Set_Gyro_ON()
+    IMU_Set_Temp_ON()
+    IMU_921600()
+
+def IMU_Set_Baudrate(baudrate) :
+    IMU_serial = serial.Serial('/dev/ttyAMA0', baudrate, parity='N', timeout=0.001)
 def IMU_reset() :
     IMU_serial.write(b'<reset>\r\n')
+def IMU_Cfg() :
+    IMU_serial.write(b'<cfg>')
 def IMU_Output_Rate_polling() :
     IMU_serial.write(b'<sor0>\r\n')
 def IMU_115200() :
     IMU_serial.write(b'<sb5>\r\n')
 def IMU_921600() :
     IMU_serial.write(b'<sb8>\r\n')
+
+# --------------------- Sensor Setting ---------------------------
+
 def IMU_Set_Acc_OFF() :
     IMU_serial.write(b'<soa0>\r\n')
 def IMU_Set_Acc_ON() :
     IMU_serial.write(b'<soa1>\r\n')
+def IMU_Set_Gyro_OFF() :
+    IMU_serial.write(b'<sog0>\r\n')
+def IMU_Set_Gyro_ON() :
+    IMU_serial.write(b'<sog1>\r\n')
+def IMU_Set_Temp_OFF() :
+    IMU_serial.write(b'<sot0>\r\n')
+def IMU_Set_Temp_ON() :
+    IMU_serial.write(b'<sot1>\r\n')
+def IMU_Set_Distance_OFF() :
+    IMU_serial.write(b'<sod0>\r\n')
+def IMU_Set_Distance_Local() :
+    IMU_serial.write(b'<sod1>\r\n')
+def IMU_Set_Distance_Global() :
+    IMU_serial.write(b'<sod2>\r\n')
+def IMU_Set_Magneto_OFF() :
+    IMU_serial.write(b'<sem0>\r\n')
+def IMU_Set_Magneto_ON() :
+    IMU_serial.write(b'<sem1>\r\n')
+def IMU_Output_Euler() :
+    IMU_serial.write(b'<sof0>\r\n')
+def IMU_Output_Code_ASCII() :
+    IMU_serial.write(b'<soc1>\r\n')
+def IMU_Output_Code_HEX() :
+    IMU_serial.write(b'<soc2>\r\n')
+
+# ------------------- Accelerometer Setting -------------------
 def IMU_Set_Sens_Acc_2G() :
     IMU_serial.write(b'<ssa1>\r\n')
 def IMU_Set_Sens_Acc_4G() :
@@ -46,44 +91,19 @@ def IMU_Set_Sens_Acc_8G() :
     IMU_serial.write(b'<ssa3>\r\n')
 def IMU_Set_Sens_Acc_16G() :
     IMU_serial.write(b'<ssa4>\r\n')
-def IMU_Set_Lpfa_OFF() :
-    IMU_serial.write(b'<lpfa0>\r\n')
-def IMU_Set_Lpfa_92Hz() :
-    IMU_serial.write(b'<lpfa5>\r\n')
-def IMU_Set_Lpfa_250Hz() :
-    IMU_serial.write(b'<lpfa7>\r\n')
-def IMU_Set_Gyro_OFF() :
-    IMU_serial.write(b'<sog0>\r\n')
-def IMU_Set_Gyro_ON() :
-    IMU_serial.write(b'<sog1>\r\n')
-def IMU_Set_Lpfg_OFF() :
-    IMU_serial.write(b'<lpfg0>\r\n')
-def IMU_Set_Lpfg_92Hz() :
-    IMU_serial.write(b'<lpfg5>\r\n')
-def IMU_Set_Lpfg_250Hz() :
-    IMU_serial.write(b'<lpfg7>\r\n')
-def IMU_Set_Lpf_OFF() :
-    IMU_serial.write(b'<lpf0>\r\n')
-def IMU_Set_Lpf_92Hz() :
-    IMU_serial.write(b'<lpf5>\r\n')
-def IMU_Set_Lpf_250Hz() :
-    IMU_serial.write(b'<lpf7>\r\n')
-def IMU_Set_Output_Temp_OFF() :
-    IMU_serial.write(b'<sot0>\r\n')
-def IMU_Set_Output_Temp_ON() :
-    IMU_serial.write(b'<sot1>\r\n')
-def IMU_Set_Output_Distance_OFF() :
-    IMU_serial.write(b'<sod0>\r\n')
-def IMU_Set_Output_Distance_Local() :
-    IMU_serial.write(b'<sod1>\r\n')
-def IMU_Set_Output_Distance_Global() :
-    IMU_serial.write(b'<sod2>\r\n')
-def IMU_Set_Output_Euler() :
-    IMU_serial.write(b'<sof0>\r\n')
-def IMU_Output_Code_ASCII() :
-    IMU_serial.write(b'<soc1>\r\n')
-def IMU_Output_Code_HEX() :
-    IMU_serial.write(b'<soc2>\r\n')
+
+# ------------------- Gyroscope Setting -------------------
+
+def IMU_Set_Sens_Gyro_250dps() :
+    IMU_serial.write(b'<ssg1>\r\n')
+def IMU_Set_Sens_Gyro_500dps() :
+    IMU_serial.write(b'<ssg2>\r\n')
+def IMU_Set_Sens_Gyro_1000dps() :
+    IMU_serial.write(b'<ssg3>\r\n')
+def IMU_Set_Sens_Gyro_2000dps() :
+    IMU_serial.write(b'ssg4>\r\n')
+
+# ------------------------- Operation -------------------------
 
 def IMU_Op() :
 
